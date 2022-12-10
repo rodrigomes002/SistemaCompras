edec7eb443bd572a410e866fa5ba5df7d3cba1d7
Rodrigo Gomes Calixto da Silva <rodrigo.calixto@radixeng.com.br>
2022-12-10 00:27:35 -0300

    implementando repository

---
+++ SistemaCompra.Infra.Data/SolicitacaoCompra/SolicitacaoCompraRepository.cs
@@ -0,0 +1,18 @@
+﻿using SistemaCompra.Domain.SolicitacaoCompraAggregate;
+
+namespace SistemaCompra.Infra.Data.SolicitacaoCompra
+{
+    public class SolicitacaoCompraRepository : ISolicitacaoCompraRepository
+    {
+        private readonly SistemaCompraContext context;
+
+        public SolicitacaoCompraRepository(SistemaCompraContext context)
+        {
+            this.context = context;
+        }
+        public void RegistrarCompra(Domain.SolicitacaoCompraAggregate.SolicitacaoCompra solicitacaoCompra)
+        {
+            context.Set<Domain.SolicitacaoCompraAggregate.SolicitacaoCompra>().Add(solicitacaoCompra);
+        }
+    }
+}
