5185eead2247026c3d1202da561788b3117eb139
Rodrigo Gomes Calixto da Silva <rodrigo.calixto@radixeng.com.br>
2022-12-10 00:27:56 -0300

    criando command e handler

---
+++ SistemaCompra.Application/SolicitacaoCompra/Command/RegistrarCompra/RegistrarCompraCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using SistemaCompra.Domain.SolicitacaoCompraAggregate;
+using SistemaCompra.Infra.Data.UoW;
+using System.Threading;
+using System.Threading.Tasks;
+using SolicitacaoAgg = SistemaCompra.Domain.SolicitacaoCompraAggregate;
+
+
+namespace SistemaCompra.Application.SolicitacaoCompra.Command.RegistrarCompra
+{
+    public class RegistrarCompraCommandHandler : CommandHandler, IRequestHandler<RegistrarCompraCommand, bool>
+    {
+        private readonly ISolicitacaoCompraRepository solicitacaoRepository;
+
+        public RegistrarCompraCommandHandler(ISolicitacaoCompraRepository solicitacaoRepository, IUnitOfWork uow, IMediator mediator) : base(uow, mediator)
+        {
+            this.solicitacaoRepository = solicitacaoRepository;
+        }
+
+        public Task<bool> Handle(RegistrarCompraCommand request, CancellationToken cancellationToken)
+        {
+            var solicitacaoCompra = new SolicitacaoAgg.SolicitacaoCompra(request.UsuarioSolicitante, request.NomeFornecedor);
+            solicitacaoRepository.RegistrarCompra(solicitacaoCompra);
+
+            Commit();
+            PublishEvents(solicitacaoCompra.Events);
+
+            return Task.FromResult(true);
+        }
+    }
+}
